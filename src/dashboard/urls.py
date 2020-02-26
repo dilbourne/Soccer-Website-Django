bfc588099dcfaa4595ad9838af44bc8628c0a4d7
Dillon Bourne <dillon_bourne82@live.com>
2020-02-26 17:22:31 -0500

    fix gitignore

---
+++ src/dashboard/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('notes/', include(('notepad.urls','notepad'))),
+    path('news/', include(('news.urls','news'))),
+    path('django_plotly_dash/', include('django_plotly_dash.urls')),
+    path('stats/',include(('stats.urls','stats')))
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
